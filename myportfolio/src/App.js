941b8dadf61590b33902bbaeb8c8c6f4c5936093
yashsowdam <yashsowdam91@gmail.com>
2025-06-28 23:47:25 +1000

    Test2

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Test from './test';
+import First from './First';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <First/>
     </div>
   );
 }
