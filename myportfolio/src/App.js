59519da33a274dc2b2d85f60250ec74f8acdd92c
yashsowdam <yashsowdam91@gmail.com>
2025-06-29 17:21:01 +1000

    Updated Sklt

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,12 +1,10 @@
 import Home from './Home';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      
     </div>
   );
 }
